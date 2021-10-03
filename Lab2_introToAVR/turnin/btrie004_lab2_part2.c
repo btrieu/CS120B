20e6213754a2406e9954039ec647d2c072f3d834
btrieu <counspls@gmail.com>
2021-10-02 23:54:29 -0700

    final ex1-2 and inital commit ex3

--- Lab2_introToAVR/turnin/btrie004_lab2_part2.c
+++ Lab2_introToAVR/turnin/btrie004_lab2_part2.c
@@ -1,8 +1,8 @@
-/*	Author: lab
+/*	Author: Brandon Trieu btrie004@ucr.edu
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+ *	Lab Section: 023
+ *	Assignment: Lab #2  Exercise #2
+ *	Exercise Description: Available Parking Spaces
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -15,9 +15,9 @@
 int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
-	unsigned char cntavail = 4;
 	unsigned char tempA = 0x00;
 while(1) {
+	unsigned char cntavail = 4;
 	tempA = (PINA & 0x0F);
 	if ((tempA & 0x01) == 0x01) {
 		cntavail--;
