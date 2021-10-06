2d5dc5996690204da9b14461d90f43481171b280
btrieu <counspls@gmail.com>
2021-10-05 22:14:58 -0700

    added lab4 ex5 files

---
+++ Lab3_bitManipulation/turnin/btrie004_lab3_part5.c
@@ -0,0 +1,31 @@
+/*	Author: Brandon Trieu btrie004@ucr.edu
+ *  Partner(s) Name: 
+ *	Lab Section: 023
+ *	Assignment: Lab #3  Exercise #2
+ *	Exercise Description: Basic car fuel sensor
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRB = 0x00; PORTB = 0x01;
+	DDRD = 0xFF; PORTD = 0xFF;
+	unsigned short weight;
+    /* Insert your solution below */
+    while (1) {
+    	weight = 0;
+	weight = ((weight + PIND) << 1) | (PINB & 0x01);
+	if (weight >= 70) {
+		PORTB = PINB | 0x02;
+	}
+	if (weight > 5 && weight < 70) {
+		PORTB = PINB | 0x04;
+	}
+    }
+    return 1;
+}
